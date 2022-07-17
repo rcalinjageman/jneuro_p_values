dd23f7cbdebaaf97ec8f3790c86972193172148d
Robert J. Calin-Jageman <rcalinjageman@gmail.com>
2022-07-17 18:46:43 -0500

    update download

--- jneuro_02_get_pdfs.R
+++ jneuro_02_get_pdfs.R
@@ -1,21 +1,28 @@
 # Libraries
 
 # Settings / Options
-jneurosci_url <- "https://www.jneurosci.org"
+jneurosci_url <- "https://www.jneurosci.org/content/jneuro/"
 articles_file <- "jneuro_articles.csv"
-volume_suffix <- "/content/"
 paper_suffix <- ".full.pdf"
 
 # Initialization
 articles <- read.csv(articles_file)
 
+articles$full_url <- paste(
+  jneurosci_url,
+  articles$url,
+  paper_suffix,
+  sep = ""
+)
+
+topaste <- data.frame(url = articles$full_url)
+write.csv(topaste, "justurl.csv")
 
 # Get each pdf, unless it already exists
 x <- 1
 for (x in 1:nrow(articles)) {
   article_url <- paste(
     jneurosci_url,
-    volume_suffix,
     articles[[x, "url"]],
     paper_suffix,
     sep = ""
@@ -33,8 +40,7 @@ for (x in 1:nrow(articles)) {
     download.file(
       url = article_url,
       destfile = destination,
-      cacheOK = FALSE,
-      method = "wininet"
+      cacheOK = FALSE
     )
   }
 }
